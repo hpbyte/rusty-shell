9fd28684819fc05231e38ab38f0b4aee5e9bd1f9
Htoo Pyae <htoopyae.dev@gmail.com>
2024-03-20 15:04:13 +0700

    feat: pipes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::io::*;
 use std::path::Path;
-use std::process::Command;
+use std::process::{Child, Command, Stdio};
 
 fn main() {
     loop {
@@ -12,25 +12,63 @@ fn main() {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
+        // piped commands
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut prev_command = None;
 
-        match command {
-            "cd" => {
-                let dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprint!("{}", e);
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                // [cd has to be a built-in to the shell](https://unix.stackexchange.com/questions/38808/why-is-cd-not-a-program/38809#38809)
+                "cd" => {
+                    let dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprint!("{}", e);
+                    }
+
+                    prev_command = None;
+                }
+                // exit from the shell
+                "exit" => return,
+                command => {
+                    let stdin = prev_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+
+                    let stdout = if commands.peek().is_some() {
+                        // more commands piped after this, send the output to the next
+                        Stdio::piped()
+                    } else {
+                        // no more commands piped after this, so display the output
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(child) => {
+                            prev_command = Some(child);
+                        }
+                        Err(e) => {
+                            eprintln!("{}", e);
+                            prev_command = None;
+                        }
+                    }
                 }
             }
-            "exit" => return,
-            command => {
-                if let Ok(mut child) = Command::new(command).args(args).spawn() {
-                    // don't accept another command until the current one completes
-                    let _ = child.wait();
-                }
-            }
+        }
+
+        if let Some(mut final_command) = prev_command {
+            // don't accept another command until the current one completes
+            let _ = final_command.wait();
         }
     }
 }
