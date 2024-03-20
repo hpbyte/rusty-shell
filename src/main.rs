5ac7bd34b522da756d0f7bf8199e86fb1e423b88
Htoo Pyae <htoopyae.dev@gmail.com>
2024-03-20 14:27:57 +0700

    feat: simplest shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::*;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("> ");
+        stdout().flush().unwrap();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        let mut child = Command::new(command).args(args).spawn().unwrap();
+
+        // don't accept another command until the current one completes
+        let _ = child.wait();
+    }
 }
